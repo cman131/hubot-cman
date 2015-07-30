cb7cb1f000fc5c36c2015fa6d65c410956ca6576
Wright <cwright@constantcontact.com>
2015-07-30 13:22:06 -0400

    changed to js

--- src/lastfm.js
+++ src/lastfm.js
@@ -23,31 +23,40 @@
 // Re-write Author:
 //   DatGuyOverThere <noreply@ultipro.com>
 
-getSong = (msg, user) ->
+function getSong(msg, user) {
   msg.http('http://ws.audioscrobbler.com/2.0/?')
-    .query(method: 'user.getrecenttracks', user: user, api_key: process.env.HUBOT_LASTFM_API_KEY, format: 'json')
-    .get() (err, res, body) ->
-      results = JSON.parse(body)
-      if results.error
-        msg.send results.message
-        return
-      song = results.recenttracks.track[0]
-      msg.send song.name + ' by ' + song.artist['#text']
-
-getMap = (user) ->
-  return user
-
-setMap = (msg, lastfmUser) ->
-  user = msg.message.user.name
-  maaaaaaps = robot.brain.get('maaaaaaps') || []
-  maaaaaaps[user] = lastfmUser
-  robot.brain.set maaaaaaps, maaaaaaps
-
-module.exports = function (robot) {
-  robot.respond /what'?s (.*) playing/i, (msg) ->
-    getSong(msg, msg.match[2])
-  robot.respond /what(')?s playing/i, (msg) ->
-    getSong(msg, getMap(msg.message.user.name))
-  robot.respond /my lastfm is (.*)/i, (msg) ->
-    setMap(msg, msg.match[1])
+    .query({method: 'user.getrecenttracks', user: user, api_key: process.env.HUBOT_LASTFM_API_KEY, format: 'json'})
+    .get(function(err, res, body) {
+      results = JSON.parse(body);
+      if (results.error) {
+        msg.send(results.message);
+        return;
+      }
+      song = results.recenttracks.track[0];
+      msg.send(song.name + ' by ' + song.artist['#text']);
+    });
 }
+
+function getMap(user) {
+  return user;
+}
+
+function setMap(msg, lastfmUser) {
+  user = msg.message.user.name;
+  maaaaaaps = robot.brain.get('maaaaaaps') || [];
+  maaaaaaps[user] = lastfmUser;
+  robot.brain.set(maaaaaaps, maaaaaaps);
+}
+
+
+module.exports = function(robot) {
+  robot.respond(/what'?s (.*) playing/i, function(msg) {
+    getSong(msg, msg.match[2]);
+  });
+  robot.respond(/what(')?s playing/i, function(msg) {
+    getSong(msg, getMap(msg.message.user.name));
+  });
+  robot.respond(/my lastfm is (.*)/i, function(msg) {
+    setMap(msg, msg.match[1]);
+  });
+};
